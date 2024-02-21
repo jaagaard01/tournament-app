7f23f5c75cf1df7e97d10fc84970841e300a40e9
John Aagaard <47125152+jaagaard01@users.noreply.github.com>
2024-02-21 15:22:20 -0500

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn">Button</button>
+    </>
+  );
+}
+
+export default App;
